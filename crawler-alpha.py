5791d48ecc78cbd00a44c680503df78694eb7b62
Christopher Bilger <christopherbilg@gmail.com>
2019-11-15 09:11:12 -0500

    Added connection to redis error checking

--- crawler-alpha.py
+++ crawler-alpha.py
@@ -1,26 +1,37 @@
 from bs4 import BeautifulSoup
 import requests
-from time import time
+import redis
 
 REDDIT = "https://www.reddit.com"
-LINKS = ["https://www.reddit.com"]
-EXTERNAL_LINKS = []
 PROCESSED_LINKS = []
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
 }
-PAGES_PROCESSED = 0
 
-
-def addToList(currentList, processedList, value):
+def addToList(processedList, value):
     if value not in processedList:
-        currentList.append(value)
+        REDIS.lpush("unprocessedLinks", str(value))
     return
 
-while LINKS:
-    t0 = time()
-    link = LINKS.pop(0)
+def getFromList():
+    if REDIS.llen("unprocessedlinks") == 0:
+        return REDDIT
+    return str(REDIS.rpop("unprocessedlinks"))
 
+# Open link to redis and check that connection has been made
+REDIS = redis.Redis(host='localhost', port=6379, db=0)
+def redisCheckConnection(maxRetries):
+    try:
+        response = rs.client_list()
+    except redis.ConnectionError:
+        if maxRetries > 0:
+            return redisCheckConnection(maxRetries - 1)
+        else:
+            return False
+    return True
+
+while True:
+    link = getFromList()
     try:
         r = requests.get(link, headers=HEADERS)
     except requests.exceptions.RequestException as e:
@@ -39,14 +50,14 @@ while LINKS:
             # Full URL Links
             if "reddit.com" in href:
                 # Internal
-                addToList(LINKS, PROCESSED_LINKS, href)
+                addToList(PROCESSED_LINKS, href)
             else:
                 # External
-                addToList(EXTERNAL_LINKS, PROCESSED_LINKS, href)
+                continue
         elif href.startswith("/"):
             # Internal "good" links
             newLink = REDDIT + href
-            addToList(LINKS, PROCESSED_LINKS, newLink)
+            addToList(PROCESSED_LINKS, newLink)
         elif "javascript" in href and "void" in href and not "/" in href:
             # Ignore javascript function calls
             continue
@@ -56,8 +67,4 @@ while LINKS:
         else:
             print(href)
 
-    t1 = time()
-    diff = t1 - t0
-    PAGES_PROCESSED = PAGES_PROCESSED + 1
-            
-    print(str(PAGES_PROCESSED) + ": " + str(len(LINKS)) + " | " + str(diff) + " seconds")
+    print(".")
